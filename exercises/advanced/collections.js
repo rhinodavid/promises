5ab271143c62eb69ebcf6c7a84c5a1aabb722b38
rhinodavid <dawalsh@gmail.com>
2016-10-06 20:53:57 -0700

    Complete combineFirstLineOfManyFiles

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,10 +7,25 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
+var Promise = require('bluebird');
+var pluckFirstLineFromFileAsync = require('../bare_minimum/promiseConstructor').pluckFirstLineFromFileAsync;
+var fs = require('fs');
 
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+  return Promise.all(
+    filePaths.map(function(path) {
+      return pluckFirstLineFromFileAsync(path);
+    })
+  )
+  .then(function(firstLines) {
+    return firstLines.join('\n');
+  })
+  .then(function(body) {
+    fs.writeFileSync(writePath, body);
+  })
+  .catch(console.log.bind(console));
+
 };
 
 // Export these functions so we can unit test them
